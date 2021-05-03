21dfe80a719258ea1cbc82524ab660c05ae70021
Phamdanghai <haipham20002000@gmail.com>
2021-05-03 18:14:56 +0700

    javademo

--- CoffeeMintClient/src/main/java/com/green/jdevd010/CoffeeMintClient/CoffeeMintClientApplication.java
+++ CoffeeMintClient/src/main/java/com/green/jdevd010/CoffeeMintClient/CoffeeMintClientApplication.java
@@ -2,7 +2,6 @@ package com.green.jdevd010.CoffeeMintClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CoffeeMintClientApplication {
